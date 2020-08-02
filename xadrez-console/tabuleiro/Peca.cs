4f87d280c65c02ec44d6def972eeb415397ed768
Lucas de Mello <lucas_mello6@hotmail.com>
2020-08-02 20:56:21 -0300

    Start to implement the board

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteDeMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QteDeMovimentos = 0;
+            Tab = tab;
+        }
+    }
+}
