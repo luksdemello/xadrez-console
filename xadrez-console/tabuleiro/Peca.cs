3a16a4d78a8af918c6320732c964b9f9c6c1897d
Lucas de Mello <lucas_mello6@hotmail.com>
2020-08-02 22:50:41 -0300

    Place pieces on the board

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -7,9 +7,9 @@
         public int QteDeMovimentos { get; protected set; }
         public Tabuleiro Tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
             QteDeMovimentos = 0;
             Tab = tab;
